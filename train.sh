d85310c8ced03f1cd92407330867457e908dc3be
liuqh16 <lqh20@mails.tsinghua.edu.cn>
2024-11-12 19:26:47 +0800

    refactor: use PPOTrainer instead of BaseRLTrainer as super class

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 TRAINER_NAME=ddppo-tdd  # ddppo|ddppo-icm|ddppo-e3b|ddppo-rnd|ddppo-noveld|ddppo-tdd
 SEED=100
-ROOT_DIR="data/hm3d/$TRAINER_NAME/train/seed=$SEED"
+ROOT_DIR="data/hm3d/$TRAINER_NAME/train/seed\=$SEED"
 
 MAGNUM_LOG=quiet HABITAT_SIM_LOG=quiet CUDA_VISIBLE_DEVICES=0 python -u -m habitat_baselines.run \
     --config-name=pointnav/ddppo_pointnav_hm3d.yaml \
     habitat_baselines.trainer_name=$TRAINER_NAME \
     habitat.seed=$SEED \
-    habitat_baselines.tensorboard_dir="$ROOT_DIR/tb" \
+    habitat_baselines.tensorboard_dir=$ROOT_DIR \
     habitat_baselines.eval_ckpt_path_dir="$ROOT_DIR/checkpoints" \
     habitat_baselines.checkpoint_folder="$ROOT_DIR/checkpoints" \
     habitat_baselines.log_file="$ROOT_DIR/train.log" \
